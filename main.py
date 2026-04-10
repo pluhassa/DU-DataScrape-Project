f62baa714bc76bf3f219b33f6ab3eb1cce8cbc06
Kyle Hermanson <kyle.hermanson@du.edu>
2026-04-10 14:23:28 -0600

    Had to create a new virtiual environment to successfully install protobuf into the python system. From there, I made a simple test.proto file that had config messages. Using that, I was able to create the test_pb2.py file by running "protoc --python_out=. test.proto" which is just the python bindings. I then made a config.txtpb which is sample data for the mean time. After that I adjusted main.py to load the protobuf data and output it and it seemed to workout just fine.

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import argparse
-
-# Argument parser setup
-parser = argparse.ArgumentParser(description='A simple Python script with command-line arguments')
-parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
-parser.add_argument('--proto', type=str, help='Path to the .proto file to use') # Run "pip install protobuf" for the library
-args = parser.parse_args()
+from google.protobuf import text_format
+import test_pb2
 
 
 def main():
+    parser = argparse.ArgumentParser(description='A simple Python script with command-line arguments')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+    parser.add_argument('--proto', type=str, help='Path to the .proto file to use')
+    parser.add_argument('--config', type=str, default='config.txtpb', help='Path to txtpb config file')
+
+    args = parser.parse_args()
+
     if args.verbose:
         print("Verbose mode enabled")
+
     print("Hello, World!")
+
+    # PROTOBUF LOADING HERE
+    config_list = test_pb2.ConfigList()
+
+    with open(args.config, "r") as f:
+        text_format.Parse(f.read(), config_list)
+
+    if args.verbose:
+        print("\nLoaded configs:")
+
+    for config in config_list.configs:
+        print(config.name, config.value)
+
 
 if __name__ == '__main__':
     main()
