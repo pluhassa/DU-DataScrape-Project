1de5432f10fed606d420c7dc9a8c02cdf581640f
Kyle Hermanson <kyle.hermanson@du.edu>
2026-04-10 12:16:29 -0600

    Attempting to add some of the protobuff information into the documents. Added the actual CLI input for proto along with the comment to run the installation for protobuf before trying. Added a blank test.proto file, but can't really figure out how to get information to run and be stored.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 # Argument parser setup
 parser = argparse.ArgumentParser(description='A simple Python script with command-line arguments')
 parser.add_argument('--verbose', action='store_true', help='Enable verbose output')
+parser.add_argument('--proto', type=str, help='Path to the .proto file to use') # Run "pip install protobuf" for the library
 args = parser.parse_args()
 
 
